5e4d63185c945e283ad76c2f3d3b8e264530a22f
Jahanzaib Hussain <m.jahanzaibhussain@gmail.com>
2016-09-11 22:28:46 +0500

    compelete task

--- app/src/main/java/com/jahanzaib/coffeecounter/MainActivity.java
+++ app/src/main/java/com/jahanzaib/coffeecounter/MainActivity.java
@@ -3,6 +3,9 @@ package com.jahanzaib.coffeecounter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+
+import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,12 +14,28 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    int numOfCoffee;
-    public void display(View view){
-        increament(numOfCoffee);
+    int quantity;
+
+    public void submitOrder(View view) {
+        int numOfCoffee = quantity;
+        display(numOfCoffee);
+        displayprice(numOfCoffee * 5);
+    }
+    public void display(int numOfCoffee) {
+        TextView quantityTextView = (TextView) findViewById(R.id.numOfCoffee);
+        quantityTextView.setText("" + numOfCoffee);
+    }
+    public void displayprice(int num){
+        TextView pamount = (TextView) findViewById(R.id.amount);
+        pamount.setText(NumberFormat.getCurrencyInstance().format(num));
     }
 
-    private void increament(int num){
-        numOfCoffee++;
+    public void inc(View view){
+        quantity = quantity + 1 ;
+        display(quantity);
+    }
+    public void dec(View view){
+        quantity = quantity - 1 ;
+        display(quantity);
     }
 }
